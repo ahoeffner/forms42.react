61f70888ba04edaddd18674f1934ed53577cf274
ahoeffner <alex@hoeffner.net>
2023-12-30 02:05:57 +0100

    Update Countries.js

--- src/listofvalues/Countries.js
+++ src/listofvalues/Countries.js
@@ -1,12 +1,32 @@
-//import  { ListOfValues } from "futureforms";
-import {Countries as listdata} from "../datasources/Countries";
+import { FilterStructure, ILike, ListOfValues } from "futureforms";
+import { Countries as listdata } from "../datasources/Countries";
 
 
-export class Countries //extends ListOfValues
+export class Countries extends ListOfValues
 {
 	title = "Countries";
+
+	filterPrefix = "%";
+	filterPostfix = "%";
 	datasource = new listdata();
+
 	sourcefields = ["country_id","country_name"];
 	targetfields = ["country_id","country_name"];
 	displayfields = ["country_id","country_name"];
+
+	constructor()
+	{
+		super();
+
+		this.filter = [];
+		this.filter.push(new ILike("country_id"));
+		this.filter.push(new ILike("country_name"));
+
+		this.datasource.addFilter
+		(
+			new FilterStructure()
+			.and(this.filter[0])
+			.or(this.filter[1])
+		);
+	}
 }
